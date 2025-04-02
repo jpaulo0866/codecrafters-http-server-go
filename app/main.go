dd8d7d15e65305e9fd705258181ec298b70eeb0d
João Schmidt <jschmidt@MacBook-Pro-de-Joao.local>
2025-04-02 17:11:08 -0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -11,15 +11,17 @@ var _ = net.Listen
 var _ = os.Exit
 
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	listener, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := listener.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
